74357e4d4fba528093b4b6b491fdf60eb2ae5da7
Boris Smus <boris@smus.com>
2013-01-23 12:02:06 -0800

    Update

--- transform-rotate/main.js
+++ transform-rotate/main.js
@@ -1,8 +1,23 @@
 var zoomFactor = 1;
+var panX = 0;
+var panY = 0;
+
+var scaleFactor = 1;
+var tX = 0;
+var tY = 0;
+var rX = 0;
+var rY = 0;
+var angle = 0;
+
+var activeObject = null;
 
 function initialize() {
   scene = document.querySelector('#scene');
   object = document.querySelector('#object');
+
+  // Handle keyboard events.
+  window.addEventListener('keydown', onkeydown);
+  window.addEventListener('keyup', onkeyup);
 
   // Handle mouse move and mouse wheel move from the trackpad.
   window.addEventListener('mousewheel', onmousewheel);
@@ -21,26 +36,77 @@ function onmousewheel(event) {
   var dy = event.wheelDeltaY;
   console.log('mousewheel', dx, dy);
 
-  if (dy > 0) {
-    zoomFactor = zoomFactor * 0.9;
-  } else if (dy < 0) {
-    zoomFactor = zoomFactor * 1.1;
+  if (activeObject) {
+    scale(dy);
+  } else {
+    zoom(dy);
   }
-  zoom(zoomFactor);
+
 }
 
 function onmousemove(event) {
   var dx = event.webkitMovementX;
   var dy = event.webkitMovementY;
   console.log('mousemove', dx, dy);
+
+  if (activeObject) {
+    rotate(dx, dy);
+  } else {
+    pan(dx, dy);
+  }
 }
 
 function onclick(event) {
   document.body.webkitRequestPointerLock();
 }
 
-function zoom(factor) {
-  scene.style.webkitTransform = 'scale(' + factor + ')';
+function setSceneTransform() {
+  var scale = 'scale(' + zoomFactor + ')';
+  var translate = 'translate(' + panX + 'px, ' + panY + 'px)';
+  scene.style.webkitTransform = scale + ' ' + translate;
+}
+
+function zoom(dy) {
+  if (dy > 0) {
+    zoomFactor = zoomFactor * 0.9;
+  } else if (dy < 0) {
+    zoomFactor = zoomFactor * 1.1;
+  }
+  setSceneTransform();
+}
+
+function pan(dx, dy) {
+  panX += dx;
+  panY += dy;
+  setSceneTransform();
+}
+
+function setObjectTransform() {
+  var scale = 'scale(' + scaleFactor + ')';
+  var translate = 'translate(' + tX + 'px, ' + tY + 'px)';
+  var rotate = 'rotate(' + angle + 'deg)';
+  object.style.webkitTransform = [translate, rotate, scale].join(' ');
+}
+
+function scale(dy) {
+  if (dy > 0) {
+    scaleFactor = scaleFactor * 0.9;
+  } else if (dy < 0) {
+    scaleFactor = scaleFactor * 1.1;
+  }
+  setObjectTransform();
+}
+
+function rotate(dx, dy) {
+  angle += (dx + dy) * 0.5;
+
+  setObjectTransform();
+}
+
+function translate(dx, dy) {
+  tX += dx;
+  tY += dy;
+  setObjectTransform();
 }
 
 function ontouchstart(e) {
@@ -51,11 +117,25 @@ function ontouchstart(e) {
   }
 }
 function ontouchmove(e) {
+  // TODO: Implement dragging objects.
+  e.preventDefault();
 }
 function ontouchend(e) {
   // If the target is the current target,
   if (e.target == activeObject) {
     activeObject = null;
     console.log('Unset active object');
+  }
+}
+
+var SPACEBAR = 32;
+function onkeydown(e) {
+  if (e.keyCode == SPACEBAR) {
+    activeObject = object;
+  }
+}
+function onkeyup(e) {
+  if (e.keyCode == SPACEBAR) {
+    activeObject = null;
   }
 }
