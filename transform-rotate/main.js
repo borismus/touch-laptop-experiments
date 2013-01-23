038f9da913b39e8f71b4f67102ccef386f87aae8
Boris Smus <boris@smus.com>
2013-01-23 12:03:05 -0800

    Update

--- transform-rotate/main.js
+++ transform-rotate/main.js
@@ -110,6 +110,7 @@ function translate(dx, dy) {
 }
 
 function ontouchstart(e) {
+  console.log('touchstart');
   // See if target is the object. If it is, set the target.
   if (e.target.id == 'object') {
     activeObject = e.target;
